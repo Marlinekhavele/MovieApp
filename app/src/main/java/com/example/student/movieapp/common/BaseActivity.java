aa303b1752cf02c0152d6538e0030e147e8db187
MarlineKhavele <marlinekhavele6@gmail.com>
2018-12-11 07:56:15 +0300

    login with facebook working

--- app/src/main/java/com/example/student/movieapp/common/BaseActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.student.movieapp.common;
-
-/**
- * Created by Student on 9/21/2017.
- */
-
-public class BaseActivity {
-}
--- app/src/main/java/com/example/student/movieapp/twitter/TwitterListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.student.movieapp.twitter;
-public interface TwitterListener {
-    void onTwitterError(String errorMessage);
-
-    void onTwitterSignIn(String authToken, String secret, long userId);
-}
